7dcda1b66d9402a1b9f8e026dd5b663f52057607
PauloCMoreira <pauloskx9004@gmail.com>
2022-11-18 11:05:32 -0300

    feat: transition button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function handleMove(image){
     image.style.marginBottom =  '2px';
+    image.style.transition = '0.1s'
     image.style.boxShadow = ('2px 2px 2px black');
     image.style.marginLeft =  '2px';
 }
 
 function handleOut(image){
     image.style.boxShadow = ('none');
+    image.style.transition = '0.1s'
     image.style.marginBottom =  0
     image.style.marginLeft =  0
 }
