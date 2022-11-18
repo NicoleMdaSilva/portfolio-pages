8c4eab5b0ca9fbcbccfc482c2cbc3462e8f8b5c8
PauloCMoreira <pauloskx9004@gmail.com>
2022-11-18 08:20:08 -0300

    add projects buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleMove(image){
+    image.style.marginBottom =  '2px';
+    image.style.boxShadow = ('2px 2px 2px black');
+    image.style.marginLeft =  '2px';
+}
+
+function handleOut(image){
+    image.style.boxShadow = ('none');
+    image.style.marginBottom =  0
+    image.style.marginLeft =  0
+}
